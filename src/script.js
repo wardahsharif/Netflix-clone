8c5fd28e4a58584b168c272e5c5d61ac07005cf2
wardahsharif <119267583+wardahsharif@users.noreply.github.com>
2023-02-12 00:15:33 +0100

    updated styles

--- src/script.js
+++ src/script.js
@@ -19,18 +19,5 @@ list.innerHTML = "Added😊";
 let listElement = document.querySelector(".myListButton");
 listElement.addEventListener("click", listButton);
 
-
-
-
-
-
-function theAlist() {
-
-let Alist = document.querySelector(".alist");
-let AlistInfo = "Romance, rivalry and radical mystery collide as a group of teens attend a remote island sleepaway camp in this suspenseful, supernatural drama.";
-Alist.innerHTML = AlistInfo;
-}
-
-let AlistElement = document.querySelector(".row");
-AlistElement.addEventListener("click", theAlist);
+     
 
